10f4236d508a5b4c6b04dfa3c55fbf3335274cbd
Marc Edwards <mdouglasedwards@gmail.com>
2017-08-08 20:23:43 -0400

    Added PostsNew form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,77 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
-    render() {
+    renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
         return (
-            <div>
-                PostsNew!
+            <div className={className}>
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
+        )
+    }
+
+    onSubmit(values) {
+
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>
         );
     }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    // validate the inputs from 'values' object
+    // errors property names mush match field.name
+    if (!values.title) {
+        errors.title = "Enter a title";
+    }
+    else if (!values.categories) {
+        errors.categories = "Enter a category or categories";
+    }
+    else if (!values.content) {
+        errors.content = "Enter some content";
+    }
+
+    // an empty errors obj indicates a valid form submission
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
